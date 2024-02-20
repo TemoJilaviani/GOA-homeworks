818dcc8aa86ca9c84bcc3e4aeeed64af07fbaac8
TemoJilaviani| <t.jilaviani@icloud.com>
2024-02-20 20:49:44 +0400

    second commit

---
+++ day 5/lesson5_books.py
@@ -0,0 +1,33 @@
+book1 = "georgian history1"
+book2 = "georgian history2"
+book3 = "georgian history3"
+book4 = "georgian history4"
+book5 = "georgian history5"
+book6 = "georgian history6"
+book7 = "georgian history7"
+book8 = "georgian history8"
+book9 = "georgian history9"
+book10 = "georgian history10"
+
+book1_price = 20 
+book2_price = 15
+book3_price = 25
+book4_price = 40
+book5_price = 30
+book6_price = 35
+book7_price = 55
+book8_price = 60
+book9_price = 70
+book10_price = 90
+
+print(book1,(book1_price - book1_price/100*10))
+print(book3,(book3_price - book3_price/100*10))
+print(book5,(book5_price - book5_price/100*10))
+print(book7,(book7_price - book7_price/100*10))
+print(book9,(book9_price - book9_price/100*10))
+print(book2,(book2_price - book2_price/100*20))
+print(book4,(book4_price - book4_price/100*20))
+print(book6,(book6_price - book6_price/100*20))
+print(book8,(book8_price - book8_price/100*20))
+print(book10,(book10_price - book10_price/100*20))
+
