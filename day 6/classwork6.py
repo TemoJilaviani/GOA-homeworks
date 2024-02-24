f8c671509aedc34ac018ef29b99c7ee69a264585
TemoJilaviani| <t.jilaviani@icloud.com>
2024-02-24 17:32:30 +0400

    second commit

---
+++ day 6/classwork6.py
@@ -0,0 +1,5 @@
+first_name = input("enter your first name: ")
+age = input("enter your age: ")
+last_name = input("enter your last name: ")
+
+print("Hello, my name is (first_name) (last_name) and I am (age) years old.")
---
+++ day 6/lesson6_register.py
@@ -0,0 +1,5 @@
+first_name = input("enter your first name: ")
+last_name = input("enter your last name: ")
+age = input("enter your age: ")
+
+print("Hello, my name is " + first_name + " " + last_name + " and I am " + age + " years old.")
