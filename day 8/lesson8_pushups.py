1f1dd93c63d8e27ad8cdc4c1b3d9b22b44aa01ae
TemoJilaviani| <t.jilaviani@icloud.com>
2024-03-07 12:18:54 +0400

    second commit

---
+++ day 8/lesson8_pushups.py
@@ -0,0 +1,8 @@
+required_squats = 60
+required_pushups = 100
+
+your_squats = int(input("please enter your number of squats: "))
+your_pushups = int(input("please enter your number of pushups: "))
+
+print(your_squats == required_squats or your_pushups == required_pushups)
+
