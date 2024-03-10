aa3b611a55f6d9994fa8edc62e7c1e92876e914b
TemoJilaviani| <t.jilaviani@icloud.com>
2024-03-10 16:41:20 +0400

    second commit

---
+++ day 11/classwork.py
@@ -0,0 +1,7 @@
+my_money = float(input("Enter your money: "))
+
+if my_money > 100:
+    print("You have enough money.")
+else:
+    print("You do not have enough money.")
+
---
+++ day 11/classwork2.py
@@ -0,0 +1,7 @@
+age = int(input("enter your age: "))
+
+# Check if the age is over eighteen
+if age >= 18:
+    print("you are adult.")
+else:
+    print("you are a child.")
