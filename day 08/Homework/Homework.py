703b97dcb99a9e66b467d6e1f403a96c31c2b4c3
TemoJilaviani| <t.jilaviani@icloud.com>
2024-04-14 01:29:37 +0400

    second commit\

--- day 08/Homework/Homework.py
+++ day 08/Homework/Homework.py
@@ -1,3 +1,4 @@
+#1
 required_squats = 60
 required_pushups = 100
 
@@ -5,4 +6,14 @@ your_squats = int(input("please enter your number of squats: "))
 your_pushups = int(input("please enter your number of pushups: "))
 
 print(your_squats == required_squats or your_pushups == required_pushups)
+
+
+#2
+required_weight = 50
+required_height = 170
+
+your_weight = int(input("please enter your weight: "))
+your_height = int(input("please enter your height: "))
+
+print(your_weight == required_weight or your_height == required_height)
 
