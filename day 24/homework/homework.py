6acfc40c8521a73fd812ee65eec449927c0f7aff
TemoJilaviani| <t.jilaviani@icloud.com>
2024-04-28 21:17:40 +0400

    second commit

--- day 24/homework/homework.py
+++ day 24/homework/homework.py
@@ -91,3 +91,7 @@ def likes(names):
         return f"{names[0]}, {names[1]} and {names[2]} like this"
     else:
         return f"{names[0]}, {names[1]} and {len(names) - 2} others like this"
+
+
+
+
